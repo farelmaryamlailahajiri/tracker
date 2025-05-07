d51e82daac868dcb8b961ad64a859c117342d42b
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-05-07 22:36:47 +0700

    try

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,21 @@
 <footer>
     <div class="container">
-      <div class="row">
-        <div class="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
-          <p>© Copyright 2025 JTI Tracker
-          <br>Design by Kelompok 6</a></p>
+        <div class="row">
+            <div class="col-lg-12 wow fadeIn" data-wow-duration="1s" data-wow-delay="0.25s">
+                <p>© Copyright 2025 JTI Tracker
+                    <br>Design by Kelompok 6</a>
+                </p>
+            </div>
         </div>
-      </div>
     </div>
-  </footer>
-    <!-- Include your JS scripts here if needed -->
-    <script src="{{ asset('templandingpage/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('templandingpage/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('templandingpage/assets/js/owl-carousel.js') }}"></script>
-    <script src="{{ asset('templandingpage/assets/js/animation.js') }}"></script>
-    <script src="{{ asset('templandingpage/assets/js/imagesloaded.js') }}"></script>
-    <script src="{{ asset('templandingpage/assets/js/templatemo-custom.js') }}"></script>
-    </body>
+</footer>
+<!-- Include your JS scripts here if needed -->
+<script src="{{ asset('templandingpage/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('templandingpage/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('templandingpage/assets/js/owl-carousel.js') }}"></script>
+<script src="{{ asset('templandingpage/assets/js/animation.js') }}"></script>
+<script src="{{ asset('templandingpage/assets/js/imagesloaded.js') }}"></script>
+<script src="{{ asset('templandingpage/assets/js/templatemo-custom.js') }}"></script>
+</body>
 
-    </html>
+</html>
