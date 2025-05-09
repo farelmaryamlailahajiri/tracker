307acadd1e3bf329e6df5a46a1d1ec2896d0be33
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-05-09 15:20:41 +0700

    tambah migration

---
+++ database/migrations/2025_05_08_064737_create_alumni_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alumni', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nim')->unique();
+            $table->foreignId('program_studi_id')->constrained('program_studi');
+            $table->date('tanggal_lulus');
+            $table->date('tanggal_pertama_kerja');
+            $table->date('tanggal_mulai_kerja_saat_ini');
+            $table->string('telepon');
+            $table->boolean('sudah_isi_formulir')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alumni');
+    }
+};
