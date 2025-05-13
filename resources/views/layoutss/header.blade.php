664e0bd9c0c81b2602add8e4644ad8a94ad68dc6
Dwi Septa Satria Agung <144086401+septasatria20@users.noreply.github.com>
2025-05-13 16:15:16 +0700

    Update header.blade.php

--- resources/views/layoutss/header.blade.php
+++ resources/views/layoutss/header.blade.php
@@ -18,6 +18,6 @@
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="{{ asset('tempdashboard/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('tempdashboard/css/sb-admin-2.css') }}" rel="stylesheet">
 
 </head>
