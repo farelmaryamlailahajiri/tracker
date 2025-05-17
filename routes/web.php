00a3fbae5d69494947f057ac21f1a0b6b0502db2
aidatulrosida <rosidaaidatul@gmail.com>
2025-05-17 14:53:36 +0700

    login done part 2

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AlumniController;
 use App\Http\Controllers\PenggunaAlumniController;
-use App\Http\Controllers\Auth\LoginAdminController;
 use App\Http\Controllers\AdminAuthController;
 use App\Http\Controllers\LulusanController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,19 +27,11 @@ Route::post('/form-alumni', [AlumniController::class, 'store'])->name('alumni.st
 Route::get('/pengguna-alumni', [PenggunaAlumniController::class, 'create'])->name('pengguna-alumni.create');
 Route::post('/pengguna-alumni', [PenggunaAlumniController::class, 'store'])->name('pengguna-alumni.store');
 
+// Dashboard (hanya GET untuk menampilkan halaman dashboard)
 Route::get('/dashboard', function () {
-    return view('dashboard.index'); // nama file: resources/views/dashboard.blade.php
+    return view('dashboard.index'); // Sesuaikan nama file blade yang benar
 })->name('dashboard');
 
-
-Route::post('/login', [LoginAdminController::class, 'login'])->name('admin.login.submit');
-Route::get('/admin/dashboard', [AdminAuthController::class, 'dashboard'])->name('admin.dashboard');
-Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
-
-Route::middleware(['auth:admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
-        return view('dashboard.index'); // arahkan ke view yang kamu buat
-    })->name('admin.dashboard');
-});
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/importLulusan', [LulusanController::class, 'index'])->name('dashboard.importLulusan');
