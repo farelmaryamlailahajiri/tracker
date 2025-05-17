a5941632f2f1510a2efc313181b0f324b3f2d18b
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-05-17 16:57:40 +0700

    Menambahkan model alumni

---
+++ app/Models/Alumni.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alumni extends Model
+{
+    protected $table = 'alumni';
+
+    // Jika kolom timestamp tidak digunakan
+    public $timestamps = false;
+
+    // Jika kamu ingin mengatur kolom yang bisa diisi
+    protected $fillable = ['id','nama', 'nim', 'program_studi_id', 'tahun_lulus']; // sesuaikan dengan tabelmu
+
+    public function programStudi()
+    {
+        return $this->belongsTo(ProgramStudi::class);   // FK: program_studi_id
+    }
+}
